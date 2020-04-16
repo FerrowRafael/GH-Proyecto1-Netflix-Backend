38b0c57ceb93802cffabffa258d3ba1c9860f807
Ferrowrafael <ferrowrafael@gmail.com>
2020-04-16 21:14:51 +0200

    Create Actor seeder

---
+++ OldNetflix/seeders/Actors.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const { Actors } = require('../models/');
+
+for (let page = 1; page < 21; page++) {
+axios.get(`https://api.themoviedb.org/3/person/popular?api_key=f1cbc5636aa2f2d3b7c9f1c1ca7c91de&language=en-US&page=${page}`)
+    .then(res => {
+        const resultados = res.data.results;
+        
+        for (const resultado of resultados) {
+            Actors.create({
+                name:resultado.name, 
+                id:resultado.id
+                
+
+            })
+            .then(actors => {resultado.known_for.forEach(actor => {actors.addMovie(actor.id)})})
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+              })
+            
+            
+        }
+    })
+}
